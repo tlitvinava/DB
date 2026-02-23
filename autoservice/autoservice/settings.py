5c807d17182392815a25b2560d06cf3863eb5350
tlitvinava <t.litvinava@gmail.com>
2026-02-23 17:50:03 +0300

    Fix logging encoding to UTF-8 for Russian characters

--- autoservice/autoservice/settings.py
+++ autoservice/autoservice/settings.py
@@ -259,11 +259,13 @@ LOGGING = {
             'class': 'logging.FileHandler',
             'filename': 'auth.log',
             'formatter': 'verbose',
+            'encoding': 'utf-8',
         },
         'console': {
             'level': 'INFO',
             'class': 'logging.StreamHandler',
             'formatter': 'verbose',
+            'encoding': 'utf-8',
         },
     },
     'loggers': {
